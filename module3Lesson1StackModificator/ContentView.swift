e9257958d1d7c2cd00decf64148b24d87423f4b6
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-01-21 11:37:48 +0300

    module3lesson1

--- module3Lesson1StackModificator/ContentView.swift
+++ module3Lesson1StackModificator/ContentView.swift
@@ -1,21 +1,139 @@
-//
-//  ContentView.swift
-//  module3Lesson1StackModificator
-//
-//  Created by Максим Минаков on 21.01.2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack(alignment: .center, spacing: 30) {
+                // Первый блок
+                Group {
+                    HStack {
+                        Text("Top location")
+                        Spacer()
+                        Text("Explore")
+                        Spacer()
+                        Text("Explore2")
+                    }
+                    
+                    HStack {
+                        Text("Top location")
+                        Spacer()
+                        Text("Explore")
+                        Spacer()
+                        Text("Explore2")
+                    }
+                    
+                    HStack {
+                        Text("Top location")
+                        Spacer()
+                        Text("Explore")
+                        Spacer()
+                        Text("Explore2")
+                    }
+                    
+                    Text("Max Minakov")
+                    Image(systemName: "person")
+                    HStack(alignment: .center, spacing: 20) {
+                        Text("Larisa")
+                        Text("Andrey")
+                        Text("Bogdan")
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 200, height: 200)
+                            .foregroundStyle(.gray)
+                        
+                        Circle()
+                            .frame(width: 40)
+                            .foregroundStyle(.red)
+                            .overlay {
+                                Image(systemName: "heart")
+                            }
+                    }
+                    
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.red)
+                        
+                        Image(systemName: "heart")
+                    }
+                }
+                
+                // Второй блок (дубликат первого)
+                Group {
+                    HStack {
+                        Text("Top location")
+                        Spacer()
+                        Text("Explore")
+                        Spacer()
+                        Text("Explore2")
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (alignment: .center, spacing: 20){
+                            Text("Top location")
+                            Text("Explore")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                            Text("Explore2")
+                        }
+                        
+                        HStack {
+                            Text("Top location")
+                            Spacer()
+                            Text("Explore")
+                            Spacer()
+                            Text("Explore2")
+                        }
+                    }
+                  
+                    
+                    Text("Max Minakov")
+                    Image(systemName: "person")
+                    HStack(alignment: .center, spacing: 20) {
+                        Text("Larisa")
+                        Text("Andrey")
+                        Text("Bogdan")
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 200, height: 200)
+                            .foregroundStyle(.gray)
+                        
+                        Circle()
+                            .frame(width: 40)
+                            .foregroundStyle(.red)
+                            .overlay {
+                                Image(systemName: "heart")
+                            }
+                    }
+                    
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.red)
+                        
+                        Image(systemName: "heart")
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal)
         }
-        .padding()
     }
 }
 
