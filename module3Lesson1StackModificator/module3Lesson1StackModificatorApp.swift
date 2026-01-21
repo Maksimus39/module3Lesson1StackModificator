74d9f504a4acf22231c3b21bc56c2cedec8822a4
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-01-21 09:29:48 +0300

    Initial Commit

---
+++ module3Lesson1StackModificator/module3Lesson1StackModificatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3Lesson1StackModificatorApp.swift
+//  module3Lesson1StackModificator
+//
+//  Created by Максим Минаков on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3Lesson1StackModificatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
